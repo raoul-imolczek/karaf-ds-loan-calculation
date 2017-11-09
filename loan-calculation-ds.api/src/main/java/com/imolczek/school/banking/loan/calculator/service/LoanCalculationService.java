f6102cd13cd294c2417ab2488c653d6780a9f9e9
Fabian <Fabian@LAPTOP-9N04ER76>
2017-11-09 08:05:39 +0100

    Initial commit of simple interface

---
+++ loan-calculation-ds.api/src/main/java/com/imolczek/school/banking/loan/calculator/service/LoanCalculationService.java
@@ -0,0 +1,9 @@
+package com.imolczek.school.banking.loan.calculator.service;
+
+import com.imolczek.school.banking.loan.calculator.model.LoanCalculationResult;
+
+public interface LoanCalculationService {
+
+	public LoanCalculationResult calculate();
+	
+}
---
+++ loan-calculation-ds.impl/src/main/java/com/imolczek/school/banking/loan/calculator/service/impl/LoanCalculationServiceImpl.java
@@ -0,0 +1,14 @@
+package com.imolczek.school.banking.loan.calculator.service.impl;
+
+import com.imolczek.school.banking.loan.calculator.model.LoanCalculationResult;
+import com.imolczek.school.banking.loan.calculator.service.LoanCalculationService;
+
+public class LoanCalculationServiceImpl implements LoanCalculationService {
+
+	@Override
+	public LoanCalculationResult calculate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
